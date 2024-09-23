7567004bba50f398ead33622f024dc73e614b6ea
dipto230 <diptoroy230305@gmail.com>
2024-09-23 18:14:35 +0600

    upper two button histroy and donation and  first card section done

---
+++ js/btn.js
@@ -0,0 +1,15 @@
+function toggleButton(buttonType){
+    const donationBtn = document.getElementById('donationBtn');
+    const historyBtn = document.getElementById('historyBtn');
+    donationBtn.classList.remove('bg-green-500', 'text-white');
+    donationBtn.classList.add('bg-white', 'text-black');
+    historyBtn.classList.remove('bg-green-500', 'text-white');
+    historyBtn.classList.add('bg-white', 'text-black');
+    if (buttonType === 'donation') {
+        donationBtn.classList.remove('bg-white', 'text-black');
+        donationBtn.classList.add('bg-green-500', 'text-white');
+      } else if (buttonType === 'history') {
+        historyBtn.classList.remove('bg-white', 'text-black');
+        historyBtn.classList.add('bg-green-500', 'text-white');
+      }
+}
